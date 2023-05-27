d9517cff63f0bb9d995fcd6ba0e74ed836a6bcc9
Kim Seong Hyeon <dhkdwk0215@naver.com>
2023-05-27 16:27:24 +0900

    Add files via upload

--- arduino_3dof/arduino_3dof.ino
+++ arduino_3dof/arduino_3dof.ino
@@ -1,14 +1,14 @@
+#define DirectionPin  (10u)
+#define BaudRate      (1000000ul)
+#define ID1            1
+#define ID2            2
+#define ID3            3
 
-#define TX_Enable_pin (12)  // 송신 활성화 핀
-#define RX_Enable_pin (13)  // 수신 활성화 핀
-#define DX_ID (0x07)        // 서보모터 아이디
-
-#include <SoftwareSerial.h>
+#include <AX12A.h>
 
 String RCVdata;
 String CMDdataDEG[3];
 int CMDdataVal[3] = { 2047, 2047, 2047 };
-SoftwareSerial mySerial(2, 3);
 float deg2rad = M_PI / 180.0;
 float rad2deg = 180.0 / M_PI;
 float b1[3], b2[3], b3[3], p1[3], p2[3], p3[3];
@@ -24,10 +24,6 @@ float z0 = 26.0; //모터 좌표
 float k; //모터 좌표 반지름
 int angle_step;
 
-typedef enum _com_mode {
-  RX_MODE,
-  TX_MODE
-} com_mode;
 
 float dot_product(float v[], float u[]) {
   float result = 0.0;
@@ -87,132 +83,7 @@ int step_transform(float length) {
   //각각의 길이를 0~4095로 변환후 반환
 }
 
-// 통신 모드(송/수신)에 따른 버퍼칩 설정
-void set_com_mode(com_mode mode) {
 
-  if (mode == RX_MODE) {
-    // 비활성화 먼저 수행하여 동시에 활성화 되는 순간을 방지
-    digitalWrite(TX_Enable_pin, LOW);   // TX disable
-    digitalWrite(RX_Enable_pin, HIGH);  // RX Enable
-  } else {
-    // 비활성화 먼저 수행하여 동시에 활성화 되는 순간을 방지
-    digitalWrite(RX_Enable_pin, LOW);   // RX disable
-    digitalWrite(TX_Enable_pin, HIGH);  // TX Enable
-  }
-}
-
-// 통신 프로토콜에 체크섬 삽입
-void dx_insert_checksum_byte(unsigned char *packet) {
-
-  unsigned char i;
-  unsigned char checksum_pt;
-  unsigned char checksum;
-  unsigned char packet_length;
-
-  packet_length = packet[3];  // 3번 바이트에 패킷 길이가 저장되어 있음
-  checksum_pt = packet_length + 3;
-
-  checksum = 0x00;
-  for (i = 2; i < checksum_pt; i++) {
-    checksum += packet[i];
-  }
-  packet[checksum_pt] = ~checksum;
-}
-
-// id 설정을 위한 통신 패킷 조립
-void dx_set_id(unsigned char id) {
-
-  unsigned char packet[8];
-  unsigned char i;
-
-  packet[0] = 0xFF;
-  packet[1] = 0xFF;
-  packet[2] = 0xFD;
-  packet[3] = 0x00;
-  packet[4] = 254;
-  packet[5] = 0x04;
-  packet[6] = 0x00;
-  packet[7] = 0x03;
-  packet[8] = id;
-
-  dx_insert_checksum_byte(packet);
-
-  set_com_mode(TX_MODE);  // 송신 모드로 설정
-  for (i = 0; i < 8; i++) {
-    Serial.write(packet[i]);
-  }
-
-  // 송신완료시까지 블록
-  while (!(UCSR0A & (1 << TXC0))) {
-    __asm__("nop\n\t");  // no operation
-  }
-
-  set_com_mode(RX_MODE);  // 수신 모드로 설정
-}
-
-// 제어 모드 설정
-// 정방향과 역방향 최대 각도에 따라 바퀴모드나 관절모드로 설정됨
-// 바퀴모드: 모두 0으로 설정
-// 관절모드: 0~360(0xfff)로 설정
-void dx_set_control_mode(unsigned char id,
-                         unsigned char cw_angle_limit[2],
-                         unsigned char ccw_angle_limit[2]) {
-
-  unsigned char packet[11];
-  unsigned char i;
-
-  packet[0] = 0xFF;
-  packet[1] = 0xFF;
-  packet[2] = id;
-  packet[3] = 0x07;
-  packet[4] = 0x03;
-  packet[5] = 0x06;
-  packet[6] = cw_angle_limit[0];
-  packet[7] = cw_angle_limit[1];
-  packet[8] = ccw_angle_limit[0];
-  packet[9] = ccw_angle_limit[1];
-  dx_insert_checksum_byte(packet);
-
-  set_com_mode(TX_MODE);  // 송신 모드로 설정
-  for (i = 0; i < 11; i++) {
-    Serial.write(packet[i]);
-  }
-
-  // 송신완료시까지 블록
-  while (!(UCSR0A & (1 << TXC0))) {
-    __asm__("nop\n\t");  // no operation
-  }
-
-  set_com_mode(RX_MODE);  // 수신 모드로 설정
-}
-
-// 위치제어용 패킷 조립
-void dx_tx_packet_for_position_control(unsigned char id, unsigned int goal_pos) {
-
-  unsigned char packet[11];
-  unsigned char i;
-  packet[0] = 0xFF;
-  packet[1] = 0xFF;
-  packet[2] = id;
-  packet[3] = 0x07;
-  packet[4] = 0x03;
-  packet[5] = 0x1E;
-  packet[6] = byte(goal_pos);
-  packet[7] = byte((goal_pos & 0x0F00) >> 8);
-  packet[8] = 0x00;
-  packet[9] = 0x00;
-  dx_insert_checksum_byte(packet);
-
-  set_com_mode(TX_MODE);  // 송신 모드로 설정
-  for (i = 0; i < 11; i++) {
-    mySerial.write(packet[i]);
-  }
-
-  delay(10);
-  set_com_mode(RX_MODE);  // 수신 모드로 설정
-}
-
-// 송신완료시까지 블록
 
 void setup() {
   //초기 위치 설정
@@ -240,28 +111,7 @@ void setup() {
 
   // put your setup code here, to run once:
   Serial.begin(115200);  // 통신 속도
-                         //  Serial.begin(115200);  // 통신 속도
-                         //  Serial.begin(19200);  // 통신 속도
-  // pinMode(TX_Enable_pin, OUTPUT);  //TX Enable
-  // pinMode(RX_Enable_pin, OUTPUT);  //RX Enable
-  mySerial.begin(57600);
-
-  // unsigned char cw_angle_limit[2];
-  // unsigned char ccw_angle_limit[2];
-
-  //  dx_set_id(DX_ID);
-  //  delay(1);
-
-  // 관절 모드로 설정
-  // cw_angle_limit[0] = 0x00;
-  // cw_angle_limit[1] = 0x00;
-  // ccw_angle_limit[0] = 0xFF;
-  // ccw_angle_limit[1] = 0x0F;
-  // //delay(1);
-
-  // dx_set_control_mode(8, cw_angle_limit, ccw_angle_limit);
-  // dx_set_control_mode(9, cw_angle_limit, ccw_angle_limit);
-  // dx_set_control_mode(10, cw_angle_limit, ccw_angle_limit);
+  ax12a.begin(BaudRate, DirectionPin, &Serial);
   delay(1000);
 }
 
@@ -315,8 +165,8 @@ void loop() {
     RCVdata = "";
 
 
-    dx_tx_packet_for_position_control(8, L1_a);
-    dx_tx_packet_for_position_control(9, L2_a);
-    dx_tx_packet_for_position_control(10, L3_a);
+    ax12a.move(ID1, L1_a);
+    ax12a.move(ID1, L2_a);
+    ax12a.move(ID1, L3_a);
   }
 }
